d31865f62fb31534eddb439eacab64600ac3eef8
Radeni <mihajlor00@gmail.com>
2024-02-14 21:27:58 +0100

    Fixed funny bug where tags werent cllosed.

--- index.php
+++ index.php
@@ -29,7 +29,30 @@ $articles = VestService::getInstance()->getArticlesByPage($page, $articlesPerPag
   $totalPages = ceil($totalArticles / $articlesPerPage);
   $articles = VestService::getInstance()->getArticlesByPageFromRubrika($page, $articlesPerPage, $currentRubrika);
   $header = RubrikaService::getInstance()->getRubrikaById($currentRubrika)->getIme();
-};
+}
+/*
+ * Truncate HTML Text: Truncates text in HTML while preserving the tags and the HTML validity 
+ * Jonas Raoni Soares da Silva <http://raoni.org>
+ * https://github.com/jonasraoni/php-truncate-html-text
+ */
+function truncate($s, $l, $e = '...', $isHTML = false, $closeTags = true){
+	$i = 0;
+	$tags = array();
+	if($isHTML){
+		preg_match_all('/<[^>]+>([^<]*)/', $s, $m, PREG_OFFSET_CAPTURE | PREG_SET_ORDER);
+		foreach($m as $o){
+			if($o[0][1] - $i >= $l)
+				break;
+			$t = substr(strtok($o[0][0], " \t\n\r\0\x0B>"), 1);
+			if($t[0] != '/' && $t[strlen($t) - 1] != '/')
+				$tags[] = $t;
+			else if(end($tags) == substr($t, 1))
+				array_pop($tags);
+			$i += $o[1][1] - $o[0][1];
+		}
+	}
+	return substr($s, 0, $l = min(strlen($s), $l + $i)) . ($closeTags && count($tags = array_reverse($tags)) ? '</' . implode('></', $tags) . '>' : '') . (strlen($s) > $l ? $e : '');
+}
 require_once 'navbar.php';
 ?>
 <!DOCTYPE html>
@@ -114,7 +137,7 @@ require_once 'navbar.php';
           <div class="card h-100 article">
             <div class="card-body">
               <h1 class="card-title"><?php echo $article->getNaslov(); ?></h1>
-              <p class="card-text"><?php echo substr($article->getTekst(), 0, 150) . '...'; ?></p>
+              <p class="card-text"><?php echo truncate($article->getTekst(), 150) . '...'; ?></p>
               <div class="d-flex justify-content-between align-items-center">
                 <div>
                   <i class="fas fa-thumbs-up"></i> <?php echo $article->getLajkovi(); ?>
